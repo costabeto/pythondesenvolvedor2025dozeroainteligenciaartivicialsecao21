8012ddcabed66ad21970a10939192af87655123f
Alberto Costa <acostaengenhariadosoftware@gmail.com>
2026-02-07 09:35:22 -0300

    Recuperando dados no HTML

--- setup/views.py
+++ setup/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'home.html')
+    nome = "Alberto"
+    pessoa = {
+        'nome': 'Maria',
+        'idade': 30,
+        'cidade': 'São Paulo'
+    }
+    return render(request, 'home.html', {'nome':nome, 'pessoa':pessoa})
 
 def about(request):
-    return render(request, 'about.html')
+    frutas = ['Maça', 'Banana', 'Laranja', 'Uva']
+    return render(request, 'about.html', {'frutas':frutas})
