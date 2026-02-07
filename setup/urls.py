4bb4302496e8395667bfe1738c50702644ff1c11
Alberto Costa <acostaengenhariadosoftware@gmail.com>
2026-02-07 10:46:09 -0300

    Criando a aplicação

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import homepage, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
-    path('about', about)
+    path('about', about),
+    path('products/', include('products.urls'))
 ]
